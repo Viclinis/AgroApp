a3b2121bbdee4316e411499c65046962f6bfe341
Viclinis <53279803+Viclinis@users.noreply.github.com>
2021-12-06 19:56:46 -0500

    inicio
    inicio

---
+++ app/src/main/java/com/example/agroapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.agroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void registroUsuario(View view){
+        Intent cambio = new Intent(this, FormularioRegistro.class);
+        startActivity(cambio);
+    }
+}
